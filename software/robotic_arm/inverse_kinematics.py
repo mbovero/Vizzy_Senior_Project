70fd0fcb3a2325f7413b76fe2a9bf48d13dccdf6
mbovero <milesbovero@icloud.com>
2025-10-18 16:45:57 -0600

    Updated servo centers

--- software/robotic_arm/inverse_kinematics.py
+++ software/robotic_arm/inverse_kinematics.py
@@ -16,7 +16,7 @@ RAD2DEG = 180.0 / pi
 DEG2RAD = pi / 180.0
 
 # --- q4 servo mapping (270°, 500–2500 µs, center 1500 µs) ---
-Q4_PWM_CENTER_US   = 830
+Q4_PWM_CENTER_US   = 1750
 Q4_PWM_MIN_US      = 500.0
 Q4_PWM_MAX_US      = 2500.0
 Q4_PWM_HALFSPAN_US = 1000.0
